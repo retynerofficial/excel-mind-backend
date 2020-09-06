7bb362de95134b77eb2dc111836f03f32706a3bd
kareem fawas <phawazzzy@gmail.com>
2020-09-06 04:21:25 +0100

    re-wrote the authmiddlewares

--- controllers/student.js
+++ controllers/student.js
@@ -1,5 +1,6 @@
 /* eslint-disable linebreak-style */
 exports.getEmail = async (req, res) => {
   const { email } = req.body;
+  console.log(req.user);
   if (email) res.status(200).send(`${email}`);
 };
