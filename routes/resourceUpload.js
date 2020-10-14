fa57ca5b84ce2592bf8be2f87734b1c8dc93c1a9
oluwaseun <dadsclement@gmail.com>
2020-10-14 10:50:19 +0100

    CRUD manipulation for resource upload

--- routes/resourceUpload.js
+++ routes/resourceUpload.js
@@ -19,4 +19,36 @@ router.post("/upload/:classid", authMiddleWare, async (req, res) => {
     res.status(500).send(err);
   }
 });
+
+router.get("/uploads", async (req, res) => {
+  const uploads = await Resource.find({});
+
+  try {
+    res.send(uploads);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.get("/upload/:classid", async (req, res) => {
+  const upload = await Resource.findOne(req.params.classid);
+
+  try {
+    res.send(upload);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.delete("/upload/:classid", async (req, res) => {
+  try {
+    const upload = await Resource.findByIdAndDelete(req.params.classid);
+
+    if (!upload) res.status(404).send("No Resource found");
+    res.status(200).send();
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 module.exports = router;
