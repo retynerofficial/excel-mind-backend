b621b912272a96e0e39d40fa46cce9243c7ab1df
BigB97 <bigbofficial01@gmail.com>
2020-10-25 00:38:37 +0100

    edit and delete class

--- controllers/classController.js
+++ controllers/classController.js
@@ -68,6 +68,62 @@ exports.createClass = async (req, res) => {
     if (!createClass) return res.status(405).json({ response: "Error creating new class" });
     return res.status(200).json({ response: createClass });
   } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error });
+  }
+};
+
+exports.updateClass = async (req, res) => {
+  try {
+    // User info from the JWT
+    const { _id } = req.user;
+
+    // Params Info
+    const { classCode } = req.params;
+
+    // Collecting the  class-name  from the body
+    const {
+      className, description, price, duration, curriculum, material
+    } = req.body;
+
+    // Check if the user input name and picture
+    if (!className) return res.status(403).json({ response: "one the fields is empty" });
+
+    // Collecting the picture-link from req.files
+    const image = req.file.path;
+    if (!image) return res.status(404).json({ error: "Image is not uploaded" });
+
+    // Saving image to cloudinary and getting back the URL
+    const imageUrl = await cloudinary.uploader.upload(
+      image,
+      (error, result) => {
+        if (error) return res.status(400).json({ error });
+        return result;
+      }
+    );
+
+    if (imageUrl) fs.unlinkSync(image);
+    const updateClass = await Class.updateOne({ classCode }, {
+      className, description, price, duration, curriculum, material
+    });
+    // check if the info was save succesfully to the DB
+    if (!updateClass) return res.status(405).json({ response: "Error updating class" });
+    return res.status(200).json({ sucess: "Sucessfully Updated" });
+  } catch (error) {
+    // console.log(error);
+    return res.status(500).json({ error });
+  }
+};
+
+exports.deleteClass = async (req, res) => {
+  try {
+    // Params Info
+    const { classCode } = req.params;
+
+    const deleteClass = await Class.deleteOne({ classCode });
+
+    return res.status(200).json({ sucess: "Deleted" });
+  } catch (error) {
     // console.log(error);
     return res.status(500).json({ error });
   }
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,9 @@ const {
   pickTest,
   chooseTest
 } = require("../controllers/testController");
-const { createClass, allClass, oneClass } = require("../controllers/classController");
+const {
+  createClass, allClass, oneClass, updateClass, deleteClass
+} = require("../controllers/classController");
 const authMiddleWare = require("../middlewares/loginAuth");
 // const parser = require("../controllers/cloudinary");
 const router = express.Router();
@@ -19,6 +21,8 @@ router.get("/test/picktest", pickTest);
 router.post("/test/:classId/create", multer().none(), chooseTest);
 router.get("/test/:course", createTest);
 router.post("/create/class", imageUpload, authMiddleWare, createClass);
+router.post("/update/class/:classCode", imageUpload, authMiddleWare, updateClass);
+router.post("/delete/class/:classCode", authMiddleWare, deleteClass);
 router.get("/course", authMiddleWare, allClass);
 router.get("/course/:classCode", authMiddleWare, oneClass);
 
