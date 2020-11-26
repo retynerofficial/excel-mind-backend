7a0848a8d1b4cc1b96cc7ce0fc6a0fa2f29d3355
kareem fawas <phawazzzy@gmail.com>
2020-11-26 13:12:19 +0100

    testing

--- routes/virtualClass.js
+++ routes/virtualClass.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const {
-  createClass, getOneVirtual, getAll, sendComment, comment
+  createClass, getOneVirtual, getAll, sendComment, comment, getComments
 } = require("../controllers/virtualClass");
 const authMiddleWare = require("../middlewares/loginAuth");
 
 const router = express.Router();
 
 router.get("/comment", comment);
+router.get("/comments", getComments);
+
 router.post("/create", authMiddleWare, createClass);
 router.get("/", authMiddleWare, getAll);
 router.get("/:id", authMiddleWare, getOneVirtual);
-router.post("/comments/:vclassid", authMiddleWare, sendComment);
+router.post("/comments/:vclassid", sendComment);
 
 module.exports = router;
