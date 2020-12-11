198f681977297bdcbc29664a115559f44af24722
oluwaseun <dadsclement@gmail.com>
2020-12-11 17:43:54 +0100

    Validate subscription API

---
+++ middlewares/validateSubscription.js
@@ -0,0 +1,17 @@
+const moment = require("moment");
+const { Payer } = require("../models/payment");
+
+const ValidateSubscription = async (req, res, next) => {
+  // Get Authenticated  User Id
+  // find time the user paid for subscription
+  // do do something like
+  const PayerInfo = await Payer.findOne({ payerId: "userId" });
+  const paymentTime = PayerInfo.paymentTimeTimestamp;
+  const expiredTime = PayerInfo.expiredTimeTimeStamp;
+  if (moment(paymentTime).diff(expiredTime, "days") > 29) {
+    next();
+  } else {
+    res.status(402).send({ error: "true", message: "Sorry, Your subscription has been expired.", result: {} });
+  }
+};
+module.exports = ValidateSubscription;
--- routes/payer.js
+++ routes/payer.js
@@ -9,7 +9,6 @@ const { initializePayment, verifyPayment } = require("../config/paystack")(reque
 const router = express.Router();
 router.use(cors());
 
-
 // Basically this route just handles the form submission and calls the paystack initializePayment function we created in our paystack module.
 // After which the response from the initializePayment() is handled: on success redirects to a receipt page or logs the error.
 
@@ -60,9 +59,17 @@ router.get("/paystack/callback", (req, res) => {
     // eslint-disable-next-line camelcase
     const [reference, amount, Course_ID, email, Student_Name] = data;
     console.log("we are checking Payer", reference, amount, Course_ID, email, Student_Name);
+
+    function addDays(theDate, days) {
+      return new Date(theDate.getTime() + days * 24 * 60 * 60 * 1000);
+    }
+    const paymentTime = new Date();
+    const expiredTime = addDays(paymentTime, 30);
+
     const newPayer = {
-      reference, amount, Course_ID, email, Student_Name
+      reference, amount, Course_ID, email, Student_Name, paymentTime, expiredTime
     };
+    console.log(newPayer);
 
     //  create a payer mongoose model object from the object just created and persist
     //   the data in the database using the save() and redirect the user to the receipt page
