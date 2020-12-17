8b6ac71abb6751a92418dd1a1f81bf306d231372
kareem fawas <phawazzzy@gmail.com>
2020-12-17 18:39:22 +0100

    added commenter name to virtual class commnts

--- models/comments.js
+++ models/comments.js
@@ -6,6 +6,7 @@ const { Schema } = mongoose;
 const commentSchema = Schema({
   virclassId: { type: Schema.Types.ObjectId, ref: "virtualClass", reqiured: true },
   commenter: { type: Schema.Types.ObjectId, ref: "users", reqiured: true },
+  commenterName: { type: String, required: true },
   comment: { type: String, required: true },
   commentType: { type: String, required: true },
   dateCreated: { type: Date, default: Date.now }
